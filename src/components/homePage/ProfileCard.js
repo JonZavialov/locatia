c260737a764a018ed98fea39d37ed0fd1b2f02c8
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2023-03-26 01:37:21 -0400

    began working on hover effect

--- src/components/homePage/ProfileCard.js
+++ src/components/homePage/ProfileCard.js
@@ -3,21 +3,35 @@ import getAge from '../../utils/getAge'
 import SocialsContainer from './SocialsContainer'
 import getImageFromUUID from '../../firebase-utils/getImageFromUUID'
 import React from 'react'
+import $ from 'jquery'
 
 function ProfileCard({profileInfo}){
-    const [uri, setUri] = React.useState('')  
+    const [uri, setUri] = React.useState('')
+    const [isHovered, setIsHovered] = React.useState(false);
+
 
     React.useEffect(() => {
         const asd = async () => {
             const response = await getImageFromUUID(profileInfo.uuid)
-            console.log(response);
             setUri(response);
         };
         asd()
     }, [profileInfo])
+
+    React.useEffect(() => {
+        if(isHovered){
+            $('#profile-cards-container').find('.profile-card:not(.hovered)').fadeTo(500, 0.2)
+        }else{
+            $('#profile-cards-container').find('.profile-card').fadeTo(200, 1)
+        }
+    }, [isHovered])
     
     return (
-        <div className="profile-card">
+        <div 
+            className={`profile-card${isHovered ? ' hovered' : ''}`}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+        >
             <img src={uri} alt={profileInfo.name} id="profile-image"></img>
             <h3>{profileInfo.name}</h3>
             <p>{profileInfo.school}</p>
