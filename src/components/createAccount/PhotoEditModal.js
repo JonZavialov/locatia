3bcab18d64dbc8ee160e76edc46043f1e8e2d6e4
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2023-04-03 11:49:40 -0400

    after hours of stuggle i have finally created a cropping system that crops the correct dimensions. i feel expteremely accomplished.

--- src/components/createAccount/PhotoEditModal.js
+++ src/components/createAccount/PhotoEditModal.js
@@ -1,5 +1,5 @@
 import Modal from 'react-modal';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import 'react-image-crop/dist/ReactCrop.css'
 import ReactCrop from 'react-image-crop';
 import 'react-image-crop/dist/ReactCrop.css'
@@ -23,13 +23,9 @@ const modalStyles = {
 };
 
 function PhotoEditModal({ src, fileObj, modalIsOpen, modalIsClosed }){
-    const [crop, setCrop] = useState({
-        unit: '%',
-        x: 25,
-        y: 25,
-        width: 50,
-        height: 50,
-    })
+    const [crop, setCrop] = useState()
+    const [croppedImage, setCroppedImage] = useState(null)
+    const imgRef = useRef(null)
 
     return (
         <Modal
@@ -39,12 +35,58 @@ function PhotoEditModal({ src, fileObj, modalIsOpen, modalIsClosed }){
             id="photo-edit-modal"
         >
             <h1>Adjust your photo</h1>
+            <p>Drag your cursor on your image to crop it</p>
             <ReactCrop crop={crop} onChange={c => setCrop(c)} aspect={1}>
-              <img src={src} alt="crop" />
+              <img src={src} alt="crop" ref={imgRef} />
             </ReactCrop>
-            <button>Crop</button>
+            {croppedImage && <img src={croppedImage} alt="cropped" />}
+            <button onClick={async() => setCroppedImage(await getCroppedImg(fileObj, imgRef.current, crop))}>Crop</button>
+            <button onClick={modalIsClosed}>close</button>
         </Modal>
     )
+}
+
+function getCroppedImg(image, imgRef, pixelCrop) {
+  return new Promise((resolve) => {
+    createImageBitmap(image).then((imageBitmap) => {
+      getImgDimensions(image).then((dimensions) => {
+        const cropScale = {
+          x: dimensions.width / imgRef.clientWidth,
+          y: dimensions.height / imgRef.clientHeight,
+        }
+
+        const canvas = document.createElement('canvas');
+        canvas.width = pixelCrop.width;
+        canvas.height = pixelCrop.height;
+        const ctx = canvas.getContext('2d');
+
+        ctx.drawImage(
+          imageBitmap,
+          pixelCrop.x * cropScale.x,
+          pixelCrop.y * cropScale.y,
+          pixelCrop.width * cropScale.x,
+          pixelCrop.height * cropScale.y,
+          0,
+          0,
+          pixelCrop.width,
+          pixelCrop.height,
+        );
+      
+        // As Base64 string
+        resolve(canvas.toDataURL('image/png'));
+      })
+    })
+  })
+}
+
+function getImgDimensions(blob){
+  return new Promise((resolve) => {
+      const img = document.createElement('img');
+      img.src = URL.createObjectURL(blob);
+      img.onload = () => {
+          resolve({width: img.width, height: img.height})
+      }
+  })
 }
 
 export default PhotoEditModal
