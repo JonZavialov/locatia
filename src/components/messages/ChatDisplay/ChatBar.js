9e1555cd6f0d381b084cf2c416bf16df523c7e94
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2023-04-14 00:27:29 -0400

    styling tweaks

--- src/components/messages/ChatDisplay/ChatBar.js
+++ src/components/messages/ChatDisplay/ChatBar.js
@@ -1,20 +1,9 @@
-import getImageFromStorage from "../../../firebase-utils/query/getImageFromStorage";
-import { useState, useEffect } from "react";
-
 function ChatBar({ chatInfo }){
-    const [sendIcon, setSendIcon] = useState(false);
-
-    useEffect(() => {
-        getImageFromStorage('/assets/send.png').then((sendIcon) => {
-            setSendIcon(sendIcon);
-        })
-    }, [])
-
     return (
         <div id="chat-bar">
             <textarea placeholder="Type a message..." />
             <button>
-                {sendIcon && <img src={sendIcon} alt="send" />}
+                SEND
             </button>
         </div>
     )
