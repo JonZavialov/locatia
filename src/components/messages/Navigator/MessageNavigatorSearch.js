b4c4c18e31626dafb5495476e6d1796570d8b1a9
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2023-04-14 23:05:39 -0400

    began working on message search

---
+++ src/components/messages/Navigator/MessageNavigatorSearch.js
@@ -0,0 +1,10 @@
+function MessageNavigatorSearch(){
+    return (
+        <div id="messages-search">
+            <i className="fa fa-search" aria-hidden="true"></i>
+            <input type="text" placeholder="Add people" />
+        </div>
+    )
+}
+
+export default MessageNavigatorSearch
--- src/components/messages/Navigator/MessagesNavigator.js
+++ src/components/messages/Navigator/MessagesNavigator.js
@@ -1,9 +1,12 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import getChatsFromUID from "../../../firebase-utils/query/getChatsFromUID";
 import  MessagesNavigatorChats  from "./MessagesNavigatorChatsContainer";
+import MessagesNavigatorSearch from "./MessageNavigatorSearch";
 
 function MessagesNavigator({ uid, onClick }){
     const [chats, setChats] = useState([]);
+    const [displayState, setDisplayState] = useState('chat');
+    const searchButtonRef = useRef();
 
     useEffect(() => {
         getChatsFromUID(uid).then((chats) => {
@@ -17,8 +20,18 @@ function MessagesNavigator({ uid, onClick }){
         <div id="chat-nav">
             <div id="chat-nav-header">
                 <h1>Messages</h1>
+                <button 
+                    ref={searchButtonRef}
+                    className={displayState === 'chat' ? '' : 'selected'} 
+                    onClick={() => {
+                        if(searchButtonRef.current.classList.contains('selected'))
+                            setDisplayState('chat')
+                        else setDisplayState('search')
+                    }}
+                >+</button>
             </div>
-            {chats ? <MessagesNavigatorChats chats={chats} onClick={onClick} />: 'No chats available!'}
+            {displayState === 'chat' && <MessagesNavigatorChats chats={chats} onClick={onClick} />}
+            {displayState === 'search' && <MessagesNavigatorSearch />}
         </div>
     )
 }
