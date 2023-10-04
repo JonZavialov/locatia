0d8193afe367f661899b4922cd8d9f86d5c9f99e
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2023-10-03 20:45:50 -0400

    Add temp connect functionality

--- src/utils/clickedContact.js
+++ src/utils/clickedContact.js
@@ -1,6 +1,7 @@
+import createNotification from "./createNotification";
+
 function clickedContact(){
-    console.log('user clicked contact button')
-    // TODO
+createNotification('error', "Error finding user! Try going to the chat tab and searching for this user.")
 }
 
 export default clickedContact;
