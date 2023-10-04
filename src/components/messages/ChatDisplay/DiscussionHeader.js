f50a47ede33f85c9854f2d4b82fa2b31e8c9f9c1
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2023-10-03 20:16:27 -0400

    Redirect to profile from chat

--- src/components/messages/ChatDisplay/DiscussionHeader.js
+++ src/components/messages/ChatDisplay/DiscussionHeader.js
@@ -1,6 +1,6 @@
 function DiscussionHeader({ userInfo }){
     return (
-        <div id="discussion-header">
+        <div id="discussion-header" onClick={() => window.location.href=`/profile/${userInfo.username}`}>
             <img src={userInfo.profilePic} alt={userInfo.name} />
             <h1>{userInfo.name}</h1>
             <p>@{userInfo.username}</p>
