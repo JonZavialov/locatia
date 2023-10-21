57bd2974a417c0b7f6bac3ca0713aa730b8a30fd
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2023-10-21 00:04:03 -0400

    Add rotating selector, not available state

--- src/components/schedule/DayOfWeekScheduler.js
+++ src/components/schedule/DayOfWeekScheduler.js
@@ -1,21 +1,27 @@
-function DayOfWeekScheduler({ day, updateAvailability, dayObject }){  
+function DayOfWeekScheduler({ day, updateAvailability, dayObject, updateNotAvailableDays, notAvailable }){  
   return (
-    <div className="hours-container">
-      {Array.from({ length: 24 }, (_, i) => i + 1).map((hour) => (
-        <p
-          className="hour"
-          key={hour}
-          onClick={() => {
-            updateAvailability(day, hour-1, !dayObject[hour-1]);
-          }}
-          style={{backgroundColor: dayObject[hour-1] ? "#B09FC4" : ""}}
-        >
-          {hour > 12 ? hour - 12 : hour}
-          &nbsp;
-          {hour > 12 ? "pm" : "am"}
-        </p>
-      ))}
-    </div>
+    <>
+      <div className={`hours-container ${notAvailable ? 'not-available' : ''}`}>
+        {Array.from({ length: 24 }, (_, i) => i + 1).map((hour) => (
+          <p
+            className="hour"
+            key={hour}
+            onClick={() => {
+              if (!notAvailable) updateAvailability(day, hour-1, !dayObject[hour-1]);
+            }}
+            style={{backgroundColor: dayObject[hour-1] ? "#B09FC4" : ""}}
+          >
+            {hour > 12 ? hour - 12 : hour}
+            &nbsp;
+            {hour > 12 ? "pm" : "am"}
+          </p>
+        ))}
+      </div>
+      <div id="not-avail-day-check">
+        <input type="checkbox" id="not-avail" checked={notAvailable ?? false} onChange={() => updateNotAvailableDays(day)}/>
+        <label htmlFor='not-avail'>I am not available on this day</label>
+      </div>
+    </>
   );
 };
 
--- src/components/schedule/Schedule.js
+++ src/components/schedule/Schedule.js
@@ -10,9 +10,15 @@ const daysOfWeek = ['Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thu
 function Schedule(){    
     const [availability, setAvailability] = useState(Object.fromEntries(daysOfWeek.map(day => [day, Array(24).fill(false)])))
     const [currentDay, setCurrentDay] = useState(daysOfWeek[0])
+    const [notAvailableDays, setNotAvailableDays] = useState([])
 
     function updateAvailability(day, hour, value) {
         setAvailability({...availability, [day]: {...availability[day], [hour]: value}})
+    }
+
+    function updateNotAvailableDays(day) {
+        if (notAvailableDays.includes(day)) setNotAvailableDays(notAvailableDays.filter(d => d !== day))
+        else setNotAvailableDays([...notAvailableDays, day])
     }
 
     useEffect(() => {
@@ -37,17 +43,23 @@ function Schedule(){
                 <h1>Create your schedule</h1>
                 <h3>Select the times that you are available</h3>
                 <div id="day-selector">
-                    <i class="fa fa-caret-left" aria-hidden="true" onClick={() => {
+                    <i className="fa fa-caret-left" aria-hidden="true" onClick={() => {
                         const index = daysOfWeek.indexOf(currentDay)
                         setCurrentDay(daysOfWeek[(index + 6) % 7])
                     }}></i>
                     <h2>{currentDay}</h2>
-                    <i class="fa fa-caret-right" aria-hidden="true" onClick={() => {
+                    <i className="fa fa-caret-right" aria-hidden="true" onClick={() => {
                         const index = daysOfWeek.indexOf(currentDay)
                         setCurrentDay(daysOfWeek[(index + 1) % 7])
                     }}></i>
                 </div>
-                <DayOfWeekScheduler day={currentDay} updateAvailability={updateAvailability} dayObject={availability[currentDay]} />
+                <DayOfWeekScheduler 
+                    day={currentDay} 
+                    updateAvailability={updateAvailability}
+                    updateNotAvailableDays={updateNotAvailableDays} 
+                    dayObject={availability[currentDay]} 
+                    notAvailable={notAvailableDays.includes(currentDay)}
+                />
                 <button onClick={() => {
                     console.log(availability)
                 }}>Save</button>
