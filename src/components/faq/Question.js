c65ef4001905e19f9a7cbacc8978459469d33642
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2023-10-21 23:08:11 -0400

    Add expandable faq

--- src/components/faq/Question.js
+++ src/components/faq/Question.js
@@ -1,8 +1,18 @@
+import { useState } from "react";
+
 function Question({title, answer}){
+    const [isExpanded, setIsExpanded] = useState(false)
+    
     return(
-        <div className="question">
-            <h3>{title}</h3>
-            <p>{answer}</p>
+        <div className="question-container">
+            <div className="question" onClick={() => setIsExpanded(!isExpanded)}>
+                <h3>{title}</h3>
+                <i 
+                    className={`fa fa-${isExpanded ? 'minus' : 'plus'}`} 
+                    aria-hidden="true"
+                ></i>
+            </div>
+            <p style={{display: isExpanded ? "" : "none"}}>{answer}</p>
         </div>
     )
 }
