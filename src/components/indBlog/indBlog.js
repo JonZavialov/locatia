d12791cacaa4d9bff59383e40befdd2506ebf359
Kade Carlson <carlsonkade3232@gmail.com>
2023-10-23 20:27:05 -0400

    added pseudocode for later this week

--- src/components/indBlog/indBlog.js
+++ src/components/indBlog/indBlog.js
@@ -8,15 +8,22 @@ function indBlog() {
     const { title } = useParams();
     const [image, setImage] = useState('');
     const [bodytext, setBodyText] = useState('');
+    const [pubdate, setPubDate] = useState('');
 
     useEffect(() => {
         // Write a query from the database here
-        // getBlofFromTitle()
+        // getBlogFromTitle()
     }, [title]);
 
     return (
         <>
             <NavBar />
+            {/* Need a div here for the body*/}
+                {/* Here will hold the image (big image probably)*/}
+                {/* publication date in small text here (font-size = 14px)*/}
+                {/* Then the title here (thinking probably size 'h3') */}
+                {/* Finally will be the body text */}
+            {/* Close div */}
             <CopyrightFooter />
         </>
     );
