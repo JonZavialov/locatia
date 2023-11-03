4f53cd83e8be702265adbee436e2ea6411190f70
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2023-11-02 23:22:34 -0400

    Add support image to contact us page

--- src/components/contactUs/ContactUs.js
+++ src/components/contactUs/ContactUs.js
@@ -12,16 +12,19 @@ function ContactUs(){
         <>
             <NavBar />
             <div id="contact-us-container">
-                <h1>Get In Touch</h1>
-                <form>
-                    <label htmlFor="name">Name</label>
-                    <input type="text" name="name" id="name" placeholder="Your Name" />
-                    <label htmlFor="email">Email</label>
-                    <input type="email" name="email" id="email" placeholder="Your Email" />
-                    <label htmlFor="message">Message</label>
-                    <textarea name="message" id="message" placeholder="Your Message"></textarea>
-                    <button onClick={(e) => submitForm(e)}>Send Message</button>
-                </form>
+                <div>
+                    <h1>Get In Touch</h1>
+                    <form>
+                        <label htmlFor="name">Name</label>
+                        <input type="text" name="name" id="name" placeholder="Your Name" />
+                        <label htmlFor="email">Email</label>
+                        <input type="email" name="email" id="email" placeholder="Your Email" />
+                        <label htmlFor="message">Message</label>
+                        <textarea name="message" id="message" placeholder="Your Message"></textarea>
+                        <button onClick={(e) => submitForm(e)}>Send Message</button>
+                    </form>
+                </div>
+                <img src={process.env.PUBLIC_URL + '/assets/support.svg'} alt="support"/>
             </div>
             <CopyrightFooter />
         </>
