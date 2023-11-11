053953a461a674f8f428323b69f65e89064deb7e
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2023-11-10 23:00:43 -0500

    Update 404 page

---
+++ src/components/notFound/NotFound.js
@@ -0,0 +1,23 @@
+import CopyrightFooter from "../footer/Footer";
+import NavBar from "../navBar/NavBar";
+import './notfound.css';
+
+function NotFound(){
+    return(
+        <>
+            <NavBar />
+            <div id="notfound-container">
+                <div>
+                    <h1>Page not found</h1>
+                    <h1>We couldn't find this page</h1>
+                    <h2>But that doesn't mean you can't find your companion!</h2>
+                    <button onClick={() => window.location.href = '/home'}>Go Home</button>
+                </div>
+                <img src={process.env.PUBLIC_URL + '/assets/404.svg'} alt="page not found" />
+            </div>
+            <CopyrightFooter />
+        </>
+    )
+}
+
+export default NotFound;
